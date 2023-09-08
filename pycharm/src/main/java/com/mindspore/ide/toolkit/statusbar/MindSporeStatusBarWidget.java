df8b9972c7528d3fadad0d5ecee6a910bef4cea4
cq <chenqing28@huawei.com>
2023-09-08 11:07:49 +0800

    pycharm, change mindspore version

---
+++ pycharm/src/main/java/com/mindspore/ide/toolkit/statusbar/MindSporeStatusBarWidget.java
@@ -0,0 +1,99 @@
+package com.mindspore.ide.toolkit.statusbar;
+
+import com.intellij.ide.plugins.PluginManagerCore;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.extensions.PluginId;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.ui.popup.ListPopup;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.wm.StatusBar;
+import com.intellij.openapi.wm.StatusBarWidget;
+import com.intellij.openapi.wm.WindowManager;
+import com.intellij.openapi.wm.impl.status.EditorBasedStatusBarPopup;
+import com.mindspore.ide.toolkit.statusbar.service.MindSporeStatusBarServiceImpl;
+import icons.MsIcons;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Slf4j
+public class MindSporeStatusBarWidget extends EditorBasedStatusBarPopup {
+    private static final String WIDGET_ID = "com.mindspore.widget";
+
+    public MindSporeStatusBarWidget(@NotNull Project project) {
+        super(project, false);
+    }
+
+    @NonNls
+    @NotNull
+    public String ID() {
+        return WIDGET_ID;
+    }
+
+    public static void update(@NotNull Project project) {
+        MindSporeStatusBarWidget widget = findWidget(project);
+        if (widget != null) {
+            widget.update(() -> widget.myStatusBar.updateWidget(WIDGET_ID));
+        }
+    }
+
+    @Nullable
+    private static MindSporeStatusBarWidget findWidget(@NotNull Project project) {
+        StatusBar bar = WindowManager.getInstance().getStatusBar(project);
+        if (bar != null) {
+            StatusBarWidget widget = bar.getWidget(WIDGET_ID);
+            if (widget instanceof MindSporeStatusBarWidget) {
+                return (MindSporeStatusBarWidget) widget;
+            }
+        }
+        return null;
+    }
+
+    @NotNull
+    protected WidgetState getWidgetState(@Nullable VirtualFile file) {
+        String version = MindSporeStatusBarServiceImpl.getCurrentSelectedVersion();
+        if (StringUtils.isEmpty(version)) {
+            version = PluginManagerCore.getPlugin(PluginId.getId("com.mindspore")).getVersion();
+            MindSporeStatusBarServiceImpl.setCurrentSelectedVersion(version);
+            if (!MindSporeStatusBarServiceImpl.getVersions().contains(version)) {
+                MindSporeStatusBarServiceImpl.addVersion(version);
+            }
+
+        }
+        EditorBasedStatusBarPopup.WidgetState state = new EditorBasedStatusBarPopup.WidgetState("", "mindspore " + version, true);
+        state.setIcon(MsIcons.MS_ICON_12PX);
+        return state;
+    }
+
+    @Nullable
+    protected ListPopup createPopup(DataContext context) {
+        DefaultActionGroup group = MindSporeStatusBarServiceImpl.createVersionActionsGroup();
+        DefaultActionGroup popupGroup = new DefaultActionGroup();
+
+        AnAction action = ActionManager.getInstance().getAction("otherVersion");
+        if (action != null) {
+            popupGroup.add(action);
+            popupGroup.addSeparator();
+        }
+        popupGroup.add(group);
+        return JBPopupFactory.getInstance()
+                .createActionGroupPopup("Api Mapping",
+                        popupGroup,
+                        context,
+                        JBPopupFactory.ActionSelectionAid.SPEEDSEARCH,
+                        false);
+    }
+
+    @NotNull
+    protected StatusBarWidget createInstance(@NotNull Project project) {
+        return new MindSporeStatusBarWidget(project);
+    }
+
+
+}
---
+++ pycharm/src/main/java/com/mindspore/ide/toolkit/statusbar/service/MindSporeStatusBarServiceImpl.java
@@ -0,0 +1,117 @@
+package com.mindspore.ide.toolkit.statusbar.service;
+
+import com.intellij.ide.lightEdit.LightEditCompatible;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.DumbAwareAction;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.util.NlsSafe;
+import com.intellij.util.ui.EmptyIcon;
+import com.mindspore.ide.toolkit.statusbar.MindSporeStatusBarWidget;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Slf4j
+public class MindSporeStatusBarServiceImpl implements MindSporeStatusBarService {
+    private static final List<String> VERSION = new CopyOnWriteArrayList<>();
+
+    private String selectedVersion;
+
+    public MindSporeStatusBarServiceImpl() {
+        ApplicationManager
+                .getApplication()
+                .getMessageBus()
+                .connect()
+                .subscribe(MindSporeStatusBarService.TOPIC, this);
+    }
+
+    public static void notifyApp(String version) {
+        ApplicationManager
+                .getApplication()
+                .getMessageBus()
+                .syncPublisher(MindSporeStatusBarService.TOPIC)
+                .mindSporeVersionChanged(version);
+    }
+
+    public static String getCurrentSelectedVersion() {
+        return ApplicationManager.getApplication().getService(MindSporeStatusBarServiceImpl.class).getVersion();
+    }
+
+    public static void setCurrentSelectedVersion(String version) {
+        ApplicationManager.getApplication().getService(MindSporeStatusBarServiceImpl.class).setVersion(version);
+    }
+
+    private String getVersion() {
+        return this.selectedVersion;
+    }
+
+    private void setVersion(String version) {
+        this.selectedVersion = version;
+    }
+
+    public static void addVersion(String version) {
+        VERSION.add(version);
+    }
+
+    public static List<String> getVersions() {
+        return VERSION;
+    }
+
+    @Override
+    public void mindSporeVersionChanged(String version) {
+        if (StringUtils.isEmpty(version)) {
+            return;
+        }
+
+        boolean shouldNotify = !version.equals(this.selectedVersion);
+        this.selectedVersion = version;
+
+        if (shouldNotify) {
+            for(Project project : ProjectManager.getInstance().getOpenProjects()) {
+                if (!project.isDisposed()) {
+                    MindSporeStatusBarWidget.update(project);
+                }
+            }
+        }
+    }
+
+    public static DefaultActionGroup createVersionActionsGroup() {
+        String selectedVersion = getCurrentSelectedVersion();
+        List<String> versions = new ArrayList<>(getVersions());
+        Collections.sort(versions);
+        versions.remove(selectedVersion);
+        DefaultActionGroup group = new DefaultActionGroup();
+        for (final String version : versions) {
+            AnAction action = new VersionAction("mindspore " + version, EmptyIcon.ICON_16) {
+                @Override
+                public void actionPerformed(@NotNull AnActionEvent e) {
+                    log.debug("choose version:{} to mapping", version);
+                    notifyApp(version);
+                }
+
+                @Override
+                public void update(@NotNull AnActionEvent e) {
+                    e.getPresentation().setEnabledAndVisible(true);
+                }
+            };
+            group.add(action);
+        }
+        return group;
+    }
+
+    private abstract static class VersionAction extends DumbAwareAction implements LightEditCompatible {
+        VersionAction(@NlsSafe String name, Icon icon) {
+            super(name, null, icon);
+        }
+    }
+}
